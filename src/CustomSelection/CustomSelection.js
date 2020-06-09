2792bec6b39373284babba966988ff55ff66806a
Ken Austria <kenaustriadev@gmail.com>
2020-06-08 22:38:22 -0700

    CustomSelection

---
+++ src/CustomSelection/CustomSelection.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./CustomSelection.css";
+
+const CustomSelection = () => {
+	return (
+		<div>
+			<p>Select This Text for Aquamarine</p>
+			<p className="example">Select This Text for Grey</p>
+		</div>
+	)
+}
+
+export default CustomSelection;
